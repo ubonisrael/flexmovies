672f95937ec4ddada12b2407b04ce021154e30e8
ubonisrael <akpanudohubonisrael@gmail.com>
2023-12-06 13:18:04 +0100

    fixed scroll bug in collection cmp

--- src/components/collection.jsx
+++ src/components/collection.jsx
@@ -15,32 +15,35 @@ export const Collection = ({
   userlist,
 }) => {
   const [x, setX] = useState(0);
-  const [showBtns, setShowBtns] = useState(false);
   const { data } = useSWR(dataURL, fetcher);
 
+  useEffect(() => {
+    if (widthRef.current) {
+      console.log(widthRef.current.offsetWidth, window.innerWidth, x)
+      console.log(widthRef.current.parentElement.offsetWidth);
+    }
+  }, [x])
   const widthRef = useRef(null);
 
   if (!data && dataURL) {
     return <CollectionSkeleton />;
   }
 
-  const handleEnter = () => setShowBtns(true);
-  const handleLeave = () => setShowBtns(false);
-
   const handleLeftScroll = () => {
     const y = widthRef.current.offsetWidth;
-    if (x - 0.25 * y >= 0) setX((prev) => prev - 0.25 * y);
+    const z = 0.5 * widthRef.current.parentElement.offsetWidth
+    if (x - (z / y) * y >= 0) setX((prev) => prev - (z / y) * y);
+    else setX(0)
   };
 
   const handleRightScroll = () => {
     const y = widthRef.current.offsetWidth;
-    if (x + 0.25 * y < y) setX((prev) => prev + 0.25 * y);
+    const z = 0.5 * widthRef.current.parentElement.offsetWidth
+    if (x + ((2 * z) / y) * y < y) setX((prev) => prev + (z / y) * y);
   };
 
   return (
     <section
-      onMouseEnter={handleEnter}
-      onMouseLeave={handleLeave}
       className={styles.collection_container}
     >
       <article className={styles.collection_type}>
@@ -52,30 +55,28 @@ export const Collection = ({
           <h2>{type}</h2>
         )}
       </article>
-      {showBtns && (
-        <>
-          <button
-            aria-hidden
-            className={styles.carousel_btns_left}
-            onClick={handleLeftScroll}
-          >
-            <FaAnglesLeft />
-          </button>
-          <button
-            aria-hidden
-            className={styles.carousel_btns_right}
-            onClick={handleRightScroll}
-          >
-            <FaAnglesRight />
-          </button>
-        </>
-      )}
+      <button
+        aria-hidden
+        className={styles.carousel_btns_left}
+        onClick={handleLeftScroll}
+      >
+        <FaAnglesLeft />
+      </button>
+      <button
+        aria-hidden
+        className={styles.carousel_btns_right}
+        onClick={handleRightScroll}
+      >
+        <FaAnglesRight />
+      </button>
       <section
         ref={widthRef}
         className={styles.collection}
         style={{
           transform: `translateX(-${
-            widthRef.current && x == 0.75 * widthRef.current.offsetWidth ? x + 4 * 16 : x
+            widthRef.current && x >= (1 - (widthRef.current.parentElement.offsetWidth / (widthRef.current.offsetWidth))) * widthRef.current.offsetWidth
+              ? (1 - (widthRef.current.parentElement.offsetWidth / (widthRef.current.offsetWidth))) * widthRef.current.offsetWidth
+              : x
           }px)`,
         }}
       >
